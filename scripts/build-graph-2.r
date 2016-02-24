2afb099418a2c9d452f287433c44943ec5247d91
Pat Smith <Pat Smith>
2016-02-23 19:34:13 -0800

    chart-2

---
+++ scripts/build-graph-2.r
@@ -0,0 +1,40 @@
+#Patrick Smith
+#Graph 2 Function
+#This function takes in a data frame and creates a chart about the 
+# passed in data set
+
+#Loading packages
+library(dplyr)
+library(plotly)
+
+#Global Options
+options(stringsAsFactors = FALSE)
+
+
+build_graph_2 <- function(data_set) {
+    
+    #Creating data frame with counts of each year of students and their interest in informatics  
+    info_by_year <- filter(data_set, Are.you.interested.in.applying.to.the.Informatics.major. == "Yes") %>% count(What.is.your.current.class.standing.) %>% inner_join(count(data_set, What.is.your.current.class.standing.), by = "What.is.your.current.class.standing.")
+    
+    #Renaming columns
+    colnames(info_by_year)[2-3] <-
+      c("Students Interested in Informatics", "Total # of Students")
+    
+    #Plotly plot settings
+    p <- plot_ly(
+      x = info_by_year$What.is.your.current.class.standing.,
+      y = info_by_year$`Students Interested in Informatics`,
+      name = 'Students interested in Info',
+      type = "bar"
+    ) %>% add_trace(
+      x = info_by_year$What.is.your.current.class.standing.,
+      y = info_by_year$`Total # of Students`,
+      name = 'Total # of Students'
+    ) %>% layout(
+      barmode = "stack",
+      xaxis = list(title = "Class Standing"),
+      yaxis = list(title = "# of Students")
+    )
+    
+    return(p)
+}
